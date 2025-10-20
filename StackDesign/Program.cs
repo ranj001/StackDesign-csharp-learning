59bc033e23e56203feb3104c47e0fce08ab4b71d
Lafeguera, Ranjit <Ranjit.Lafeguera@analog.com>
2025-10-20 16:59:40 +0800

    Code optimization

--- StackDesign/Program.cs
+++ StackDesign/Program.cs
@@ -2,32 +2,54 @@
 
 namespace StackDesign
 {
-        class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            StackOperation1(); //"Second", "First"
+            StackOperation2(); //"Third"
+            StackOperation3(); //"Fourth"
+            StackOperation4(); // "9", "8", "7", "5", Exception
+
+        }
+
+        static void StackOperation1()
+        {
             var stack = new Stacks();
-            stack.Push(1);
-            stack.Push(2);
-            stack.Push(3);
-            stack.Push(4);
-            stack.Push(5);
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine(stack.Pop());
+            stack.Push("First");
+            stack.Push("Second");
+            stack.PeekAll();
+        }
+
+        static void StackOperation2()
+        {
+            var stack = new Stacks();
+            stack.Push("Third");
+            Console.WriteLine(stack.Pop().ToString());
+        }
+
+        static void StackOperation3()
+        {
+            var stack = new Stacks();
+            stack.Push("Fourth");
+            stack.PeekAll();
+        }
+
+        static void StackOperation4()
+        {
+            var stack = new Stacks();
+            stack.Push("Fifth");
+            stack.Push("Sixth");
+            stack.Push(7);
+            stack.Push(8);
+            stack.Push(9);
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine(stack.Pop().ToString());
             Console.WriteLine(stack.Count().ToString());
-            try
-            {
-                Console.WriteLine(stack.Peek());
-                stack.Clear();
-                Console.WriteLine(stack.Count().ToString());
-                Console.WriteLine(stack.Peek());
-            }
-            catch (InvalidOperationException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            
+            stack.Clear();
+            stack.PeekAll();
+
         }
     }
 }
--- StackDesign/Stacks.cs
+++ StackDesign/Stacks.cs
@@ -13,33 +13,39 @@
        
         public void Push(object item)
         {
+            if (item == null)
+                throw new InvalidOperationException("Item cannot be null.");
+
             if (_count == _items.Length)
-            {
                 Resize(_items.Length * 2);
-            }
+
             _items[_count++] = item;
         }
         public object Pop()
         {
             if (_count == 0)
-            {
                 throw new InvalidOperationException("Stack is empty.");
-            }
+
             object item = _items[--_count];
             _items[_count] = null; // Clear the reference
+
             if (_count > 0 && _count == _items.Length / 4)
-            {
                 Resize(_items.Length / 2);
-            }
+
             return item;
         }
-        public object Peek()
+        public void PeekAll()
         {
             if (_count == 0)
-            {
                 throw new InvalidOperationException("Stack is empty.");
+
+            for (int i = _count - 1; i >= 0; i--)
+            {
+                if (_items[i] == null)
+                    break;
+                Console.WriteLine(_items[i]);
             }
-            return _items[_count - 1];
+
         }
         public void Clear()
         {
@@ -48,7 +54,7 @@
         }
         public int Count()
         { 
-            return _items.Length;
+            return _items.Length + 1;
         }
         public int StackCounter => _items.Length;
         private void Resize(int newCapacity)
